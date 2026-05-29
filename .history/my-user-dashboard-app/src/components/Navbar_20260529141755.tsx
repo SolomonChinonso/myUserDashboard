a95ed1b5121be9672dc94f7eecbd4013e98c4583
Chinonso Nnadi <nnadisolomon1@gmail.com>
2026-05-29 14:27:00 +0100

    initial commit

---
+++ .history/my-user-dashboard-app/src/components/Navbar_20260529141755.tsx
@@ -0,0 +1,34 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const Navbar = () => {
+  const { user, logout } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
+  return (
+    <nav>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/profile">Profile</Link>
+
+      {user?.role === "Admin" && (
+        <Link to="/settings">Settings</Link>
+      )}
+
+      <span>
+        {user?.username} ({user?.role})
+      </span>
+
+      <button onClick={handleLogout}>
+        Logout
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
